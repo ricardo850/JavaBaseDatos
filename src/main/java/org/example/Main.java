00ddb535f17c769fe10eaed330b9641d66673ab2
julio <richardmurillo789s@gmail.com>
2025-04-25 15:03:10 -0500

    nuevo cambio

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -18,5 +18,20 @@ public class Main {
         MetodosBaseDatos.IngresarDatos(NombreApellido,cedula);
 
 
+        System.out.println("escribe el numero de la cedula del registro que deseas actualizar:");
+
+        System.out.println("escribe la cedula:");
+        int cedulaBuscar = teclado.nextInt();
+
+        teclado = new Scanner(System.in);
+        System.out.println("Ahora escribe el nombre y apellidos nuevos:");
+        String NombreApellidoActualizar = teclado.nextLine();
+
+        teclado = new Scanner(System.in);
+        System.out.println("escribe el numero de cedula nuevo:");
+        int cedulaActualizar = teclado.nextInt();
+
+        MetodosBaseDatos.ActualizarDatos(cedulaBuscar,NombreApellidoActualizar,cedulaActualizar);
+
     }
 }
--- src/main/java/org/example/MetodoUsuario.java
+++ src/main/java/org/example/MetodoUsuario.java
@@ -1,15 +1,28 @@
 package org.example;
 
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class MetodoUsuario {
 
+    private SessionFactory sessionFactory;
+    // Variable para almacenar la sesión
+    private Session session;
+
+    // Constructor para inicializar la SessionFactory y abrir una sesión
+    public void ConfigHibernate() {
+
+        sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+        // Abrir la sesión
+        session = sessionFactory.openSession();
+    }
+
+
+
+
     public void IngresarDatos(String NombreApellido, int cedula){
-        Session session = new Configuration()
-                .configure("hibernate.cfg.xml")
-                .buildSessionFactory()
-                .openSession();
+        ConfigHibernate();
         session.beginTransaction();
         Usuario user = new Usuario(NombreApellido,cedula);
         session.save(user);
@@ -19,6 +32,26 @@ public class MetodoUsuario {
 
     }
 
+
+    public void ActualizarDatos(int cedulaActualizar, String NombreApellidoNuevo, int cedulaNueva){
+        ConfigHibernate();
+        session.beginTransaction();
+        Usuario usuario = (Usuario) session.createQuery("from Usuario where cedula = " + cedulaActualizar )
+                                .uniqueResult();
+
+        usuario.setNombreApellido(NombreApellidoNuevo);
+   usuario.setCedula(cedulaNueva);
+
+   session.update(usuario);
+
+   session.getTransaction().commit();
+   session.close();
+
+    }
+
+
+
     public MetodoUsuario() {
+
     }
 }
